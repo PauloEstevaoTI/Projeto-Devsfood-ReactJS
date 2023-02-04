1340fbd7141ee2d4f509b9e800269f6fc821fa51
Bombeta <paullo_jf@hotmail.com>
2023-02-04 15:43:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+            <h1>Oi, {name}</h1>
+
+            <Switch>
+                <Route exact path="/">
+                    <HomeScreen />
+                </Route>
+                <Route path="/tela2/:nome">
+                    <Tela2Screen />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
-
-export default App;
